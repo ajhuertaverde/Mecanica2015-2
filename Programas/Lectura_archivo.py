acccc3c3e31224bca70c9f910cf112d6372ec003
Aldo Josué Huerta Verde <ahuerta@grupoabraxas.com>
2016-02-26 10:44:55 -0600

    Se agregan videos de la clase

--- Programas/Lectura_archivo.py
+++ Programas/Lectura_archivo.py
@@ -1,11 +1,18 @@
 #En este Programa abriremos un archivo y haremos un promedio de datos.
+import math
+
 promedio = 0;
 contador = 0;
+promedioLog = 0;
 file =  open('../Datos/medidas.csv','r')
 #print file
 for linea  in file:
     columnas = linea.split(",")
     if (float(columnas[1]) > 0.0):
-       promedio += float(columnas[1])
        contador += 1;
+       promedio += float(columnas[1])
+       promedioLog += math.log(float(columnas[1]))
 print promedio/contador
+
+print promedioLog/contador
+print math.log(promedio/contador)
